cd71fbdf1824b663f7505a886bda9ac5bf60f9b4
dashersw <armagan@amcalar.com>
2011-04-09 05:54:17 +0300

    Added base.js that combines dependencies into one, namely, dashMVC.

---
+++ js/base/base.js
@@ -0,0 +1,15 @@
+/**
+ * @license dashMVC - a Javascript MVC that plays perfect with Google Closure Library
+ * @author armagan@amcalar.com (Armagan Amcalar)
+ * This is an open source project. Use it at your own risk however you want, keeping this license information intact.
+ */
+
+/**
+ * @fileoverview This file combines the requirements of dashMVC in a single requirement called dashMVC so that
+ * projects that use the dashMVC need only require dashMVC.
+ */
+
+goog.require('dashMVC.Controller');
+goog.require('dashMVC.Model');
+goog.require('dashMVC.View');
+goog.provide('dashMVC');
